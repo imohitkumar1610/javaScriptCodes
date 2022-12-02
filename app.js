28624c7f253a4585c6505d47226426b87ad75af5
Mohit <mohitkumar91614@gmail.com>
2022-12-02 15:50:18 +0530

    commit

--- app.js
+++ app.js
@@ -25,15 +25,47 @@
 // }
 
 
-const btn1 = document.querySelector('.btn1');
+const btn1 = document.querySelector('#btn1');
 const box = document.querySelector('.box');
 count = 0;
 
 function increment(){
     count+=1;
     box.innerHTML = count;
+
+// innerText returns all text contained by an element and all its  child elements. innerHtml returns all text, including html tags, that is contained by an element.
+
+//     console.log(count);
+    
 };
-btn1.addEventListener("click",increment);
+// btn1.addEventListener("click",increment);
+
+const btnSave = document.querySelector('.btn-save');
+ function save(){
+    console.log(count)
+ }
+
+// function countdown(){
+//     console.log(1);
+//     console.log(2);
+//     console.log(3);
+//     console.log(4);
+//     console.log(5);
+//     console.log(6);
+// }
+// countdown();
 
 
+// let lapsComplete = 0;
+
+// function incrementLap(){
+//     lapsComplete += 1;
+// }
+// incrementLap();
+// console.log(lapsComplete);
+
+// incrementLap();
+// console.log(lapsComplete);
+
+//everytime you envoke the function lapcount got increased by 1
 
