e84c01ebe1ffbbddfa01db35be30fa5d0018ffcd
Mohit <mohitkumar91614@gmail.com>
2022-12-02 14:18:49 +0530

    commit

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const btn1 = document.querySelector('.btn1');
+// const btn1 = document.querySelector('.btn1');
 
-function clickbtn (){
-    btn1.innerHTML = 'clicked';
-};
-btn1.addEventListener("click",clickbtn);
+// function clickbtn (){
+//     btn1.innerHTML = 'clicked';
+// };
+// btn1.addEventListener("click",clickbtn);
 
 // const btn1 = document.querySelector('.btn1');
 // console.log(btn1);
@@ -13,3 +13,27 @@ btn1.addEventListener("click",clickbtn);
 // // alert("i am an engineer");
 // };
 // btn1.addEventListener("click",clickbtn);
+
+// let firstBatch = 5;
+// let secondbatch = 7;
+// let count = firstBatch + secondbatch;
+// console.log(count);
+
+// let num = 25;
+// for(let i = 1;i<=num;i++){
+//     console.log(i);
+// }
+
+
+const btn1 = document.querySelector('.btn1');
+const box = document.querySelector('.box');
+count = 0;
+
+function increment(){
+    count+=1;
+    box.innerHTML = count;
+};
+btn1.addEventListener("click",increment);
+
+
+
