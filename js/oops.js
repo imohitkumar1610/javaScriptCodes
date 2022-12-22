9c8aa37ec3253f4d3426bbf4beade5eb4590bcd1
Mohit <mohitkumar91614@gmail.com>
2022-12-22 23:08:37 +0530

    commit

--- js/oops.js
+++ js/oops.js
@@ -2,9 +2,17 @@ console.log("this is a file for oops in javascript");
 
 function BankAccount(username , balance = 0){
     this.userName = username;
-    this.accountNumber = accNum;
+    this.accountNumber = Date.now();
     this.balance = balance;
+
+    // functions in a class is calleld methods 
+    this.deposite = function(amount){
+        this.balance = this.balance + amount;
+    }
 }
 
 const mohit = new BankAccount("Mohit", 10000);
+
+mohit.deposite(5200); // this will add 5200 amount in your bank account
+
 console.log(mohit);
